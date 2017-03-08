b6c9876a0b6f0414a8596c7f6e15e19fe7b9626e
Jacob Stevens <cobystevens@live.com>
2017-03-08 02:46:12 -0500

    Basics of GUI Finished for PEX 3
    Some adjustments may be made later to have it look better

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -13,8 +13,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.Clip;
 import javax.swing.ButtonGroup;
@@ -24,13 +22,14 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-import javax.swing.JTextField;
+import javax.swing.JTextArea;
+
 
 public class MainFrame extends JFrame
 {
 
     JPanel title = new JPanel();
-    JPanel verttitle = new JPanel(new GridLayout(1, 2));
+    JPanel verttitle = new JPanel(new GridLayout(1,2));
     ImageIcon IC = new ImageIcon();
     Clip music;
     AudioInputStream ais;
@@ -40,17 +39,17 @@ public class MainFrame extends JFrame
         //Beginning of TitleFrame
         JLabel label;
         JLabel label1;
-        //ImageIcon Cipherpic = new ImageIcon(getClass().getResource("/Picture/CRW.gif"));
-        label = new JLabel("Custom Cipher");
-        label.setFont(new Font("", Font.BOLD, 30));
-        //label1 = new JLabel(Cipherpic);
+        ImageIcon Cipherpic = new ImageIcon(getClass().getResource("/Picture/CRW.gif"));
+        label = new JLabel("    The Great Enigma Cipher  ");
+        label.setFont(new Font("", Font.BOLD, 25));
+        label1 = new JLabel(Cipherpic);
         verttitle.add(label);
-        //vert.add(label1);
+        verttitle.add(label1);
         title.add(verttitle);
         //Ending to TitleFrame
 
         
-        //Beginning of InputSource
+        //Beginning of Plugboard
         JPanel Inputchoice = new JPanel();
         String userfileinput;
         String userkeyboardinput;
@@ -58,91 +57,138 @@ public class MainFrame extends JFrame
         JLabel j10 = new JLabel();
         JLabel j11 = new JLabel();
         JLabel j12 = new JLabel();
-        JLabel jl2 = new JLabel("  Input Source");
+        JLabel jl4 = new JLabel("  Enter Plugboard Settings");
         JPanel vert2 = new JPanel();
-        ButtonGroup bg2 = new ButtonGroup();
         vert2.setLayout(new GridLayout(7, 1));
-        JRadioButton b4 = new JRadioButton("File");
-        JRadioButton b5 = new JRadioButton("Keyboard");
-        bg2.add(b4);
-        bg2.add(b5);
-        vert2.add(j9);
-        vert2.add(j10);
-        vert2.add(j11);
-        vert2.add(j12);
-        vert2.add(jl2);
-        vert2.add(b4);
-        vert2.add(b5);
+        JTextArea jta = new JTextArea(5,3);
+
+
+//        vert2.add(j9);
+//        vert2.add(j10);
+//        vert2.add(j11);
+//        vert2.add(j12);
+        vert2.add(jl4);
+        vert2.add(jta);
+
         Inputchoice.add(vert2);
-        //End of InputSource
+        //End of Plugboard
         
         
-        //Beginning of Type of Cipher
+        //Beginning of Cipher Wheels
         JPanel TOC = new JPanel();
-        JRadioButton b1 = new JRadioButton("Caesar");
-        JRadioButton b2 = new JRadioButton("Substitution");
-        JRadioButton b3 = new JRadioButton("Rotation");
+        JRadioButton b1 = new JRadioButton("Cipher One");
+        JRadioButton b2 = new JRadioButton("Cipher Two");
+        JRadioButton b3 = new JRadioButton("Cipher Three");
+        JRadioButton b4 = new JRadioButton("Cipher Four");
+        JRadioButton b5 = new JRadioButton("Cipher Five");
+        JRadioButton b6 = new JRadioButton("Cipher One");
+        JRadioButton b7 = new JRadioButton("Cipher Two");
+        JRadioButton b8 = new JRadioButton("Cipher Three");
+        JRadioButton b9 = new JRadioButton("Cipher Four");
+        JRadioButton b10 = new JRadioButton("Cipher Five");
+        JRadioButton b11 = new JRadioButton("Cipher One");
+        JRadioButton b12 = new JRadioButton("Cipher Two");
+        JRadioButton b13 = new JRadioButton("Cipher Three");
+        JRadioButton b14 = new JRadioButton("Cipher Four");
+        JRadioButton b15 = new JRadioButton("Cipher Five");
         JLabel j1 = new JLabel();
         JLabel j2 = new JLabel();
         JLabel j3 = new JLabel();
         JLabel j4 = new JLabel();
-        JLabel jl = new JLabel("  Type of Cipher");
+        JLabel j5 = new JLabel();
+        JLabel j6 = new JLabel();
+        JLabel jl1 = new JLabel("   Inner Wheel");
+        JLabel jl2 = new JLabel("   Middle Wheel");
+        JLabel jl3 = new JLabel("   Outer Wheel");
         JPanel vert = new JPanel();
-        ButtonGroup bg = new ButtonGroup();
-        vert.setLayout(new GridLayout(8, 1));
-        bg.add(b1);
-        bg.add(b2);
-        bg.add(b3);
+        ButtonGroup bg3 = new ButtonGroup();
+        ButtonGroup bg4 = new ButtonGroup();
+        ButtonGroup bg5 = new ButtonGroup();
+        
+        vert.setLayout(new GridLayout(9,3,0,15));
+        
+        bg3.add(b1);
+        bg3.add(b2);
+        bg3.add(b3);
+        bg3.add(b4);
+        bg3.add(b5);
+        
+        bg4.add(b6);
+        bg4.add(b7);
+        bg4.add(b8);
+        bg4.add(b9);
+        bg4.add(b10);
+        
+        bg5.add(b11);
+        bg5.add(b12);
+        bg5.add(b13);
+        bg5.add(b14);
+        bg5.add(b15);
+        
         vert.add(j1);
         vert.add(j2);
         vert.add(j3);
         vert.add(j4);
-        vert.add(jl);
+        vert.add(j5);
+        vert.add(j6);
+
+        
+        vert.add(jl1);
+        vert.add(jl2);
+        vert.add(jl3);
+        
         vert.add(b1);
+        vert.add(b6);
+        vert.add(b11);
+
         vert.add(b2);
+        vert.add(b7);
+        vert.add(b12);
+        
+        
         vert.add(b3);
+        vert.add(b8);
+        vert.add(b13);
+        
+        vert.add(b4);
+        vert.add(b9);
+        vert.add(b14);
+        
+        vert.add(b5);
+        vert.add(b10);
+        vert.add(b15);
+        
         TOC.add(vert);
-        //End of Type of Cipher
+        //End of Cipher Wheels
         
         
         //Beginning of encodeordecode
         JPanel endebutton = new JPanel();
         JRadioButton encode = new JRadioButton("Encode");
         JRadioButton decode = new JRadioButton("Decode");
-        JLabel j5 = new JLabel();
-        JLabel j6 = new JLabel();
-        JLabel j7 = new JLabel();
-        JLabel j8 = new JLabel();
-        JLabel jl1 = new JLabel("        Mode");
+        JLabel jspace = new JLabel();
         JPanel vert1 = new JPanel();
         ButtonGroup bg1 = new ButtonGroup();
-        vert1.setLayout(new GridLayout(7, 1));
+        vert1.setLayout(new GridLayout(3,1,0,100));
         bg1.add(encode);
         bg1.add(decode);
-        vert1.add(j5);
-        vert1.add(j6);
-        vert1.add(j7);
-        vert1.add(j8);
-        vert1.add(jl1);
+        vert1.add(jspace);
         vert1.add(encode);
         vert1.add(decode);
         endebutton.add(vert1);
         //End of encodeordecode
 
         
-        //Beginning of textfield and submit
+        //Beginning of JButton Coding
         JPanel jptextwithsubmit = new JPanel();
-        JTextField jtf = new JTextField(26);
-        jtf.setHorizontalAlignment(JTextField.LEFT);
-        jptextwithsubmit.add(jtf, BorderLayout.EAST);
-        JButton jb = new JButton("Encode/Decode");
-        jptextwithsubmit.add(jb, BorderLayout.WEST);
-        //End of textfield and submit
+        JButton jb = new JButton("Begin The Coding Process");
+        jptextwithsubmit.add(jb);
+        //End of JButton Coding
 
         
         //Full MainFrame
         setTitle("Substitution Cipher");
-        setSize(550, 500);
+        setSize(700, 700);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         //Aligns window to center of screen. Found it here: http://stackoverflow.com/questions/2442599/how-to-set-jframe-to-appear-centered-regardless-of-monitor-resolution
