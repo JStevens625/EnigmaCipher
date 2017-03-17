d1cdd6a317a553db29afb2bf6652a62bf2136799
HaloMixter <mattsonlj@gmail.com>
2017-03-16 21:48:50 -0400

    Add files via upload

--- src/Enigma.java
+++ src/Enigma.java
@@ -12,14 +12,14 @@ public class Enigma {
     String key_3 = "BDFHJLNPRTVXZACEGI#KMOQSUWY";
     String key_4 = "KPHDEAC#VTWQMYNLXSURZOJFBGI";
     String key_5 = "NDYGLQICVEZRPTAOXWBMJSUHK#F";
-    String keyForOuter;
-    String keyForMid;
-    String keyForInner;
+    StringBuilder keyForOuter;
+    StringBuilder keyForMid;
+    StringBuilder keyForInner;
     int startPosInner;
     int startPosMid;
     int startPosOuter;
 
-    public String encode(String in, int start1, int start2, int start3, int wheel_I, int wheel_M, int wheel_O, String plugboard) {
+    public String encode(String in, int start1, int start2, int start3, int wheel_I, int wheel_M, int wheel_O, String[] plugboard) {
         int length = in.length();
         in = in.replaceAll(" ", "#");
         in = in.toLowerCase();
@@ -27,57 +27,57 @@ public class Enigma {
 
         String encoded = in;
         if (wheel_O == 1) {
-            keyForOuter = key_1;
+            keyForOuter = new StringBuilder(key_1);
         } else {
             if (wheel_O == 2) {
-                keyForOuter = key_2;
+                keyForOuter = new StringBuilder(key_2);
             } else {
                 if (wheel_O == 3) {
-                    keyForOuter = key_3;
+                    keyForOuter = new StringBuilder(key_3);
                 } else {
                     if (wheel_O == 4) {
-                        keyForOuter = key_4;
+                        keyForOuter = new StringBuilder(key_4);
                     } else {
                         if (wheel_O == 5) {
-                            keyForOuter = key_5;
+                            keyForOuter = new StringBuilder(key_5);
                         }
                     }
                 }
             }
         }
         if (wheel_M == 1) {
-            keyForMid = key_1;
+            keyForMid = new StringBuilder(key_1);
         } else {
             if (wheel_M == 2) {
-                keyForMid = key_2;
+                keyForMid = new StringBuilder(key_2);
             } else {
                 if (wheel_M == 3) {
-                    keyForMid = key_3;
+                    keyForMid = new StringBuilder(key_3);
                 } else {
                     if (wheel_M == 4) {
-                        keyForMid = key_4;
+                        keyForMid = new StringBuilder(key_4);
                     } else {
                         if (wheel_M == 5) {
-                            keyForMid = key_5;
+                            keyForMid = new StringBuilder(key_5);
                         }
                     }
                 }
             }
         }
         if (wheel_I == 1) {
-            keyForInner = key_1;
+            keyForInner = new StringBuilder(key_1);
         } else {
             if (wheel_I == 2) {
-                keyForInner = key_2;
+                keyForInner = new StringBuilder(key_2);
             } else {
                 if (wheel_I == 3) {
-                    keyForInner = key_3;
+                    keyForInner = new StringBuilder(key_3);
                 } else {
                     if (wheel_I == 4) {
-                        keyForInner = key_4;
+                        keyForInner = new StringBuilder(key_4);
                     } else {
                         if (wheel_I == 5) {
-                            keyForInner = key_5;
+                            keyForInner = new StringBuilder(key_5);
                         }
                     }
                 }
@@ -86,16 +86,37 @@ public class Enigma {
         startPosInner = start1;
         startPosMid = start2;
         startPosOuter = start3;
+        int count = 0;
+        int rotate = 0;
+        int turn = 0;
+        char temporary;
+        char temp;
         for (int i = 0; i < length; i++) {
-            index = keyForInner.indexOf(in.charAt(i));
-            index = keyForMid.indexOf(keyForOuter.charAt(index));
+            index = keyForInner.toString().indexOf(in.charAt(i));
+            index = keyForMid.toString().indexOf(keyForOuter.charAt(index));
             encoded = encoded.replace(in.charAt(i), keyForOuter.charAt(index));
+            index = keyForInner.toString().toLowerCase().indexOf(in.charAt(i));
+            index = keyForMid.toString().toLowerCase().indexOf(keyForOuter.toString().toLowerCase().charAt(index));
+            encoded = encoded.replace(in.charAt(i), keyForOuter.toString().toLowerCase().charAt(index));
+            count++;
+            for(int j = 0; j<27; j++){
+                temp = keyForInner.charAt(0);
+                if(j != 26){
+                    temporary = keyForInner.charAt(j);
+                    keyForInner.setCharAt(j++, temporary);
+                }
+                else{
+                    keyForInner.setCharAt(j,temp);
+                }
+            }
+            if (count % 27 == 0) {
+                rotate++;
+            }
+            if (rotate % 27 == 0) {
+                turn++;
+            }
         }
-        for (int i = 0; i < length; i++) {
-            index = keyForInner.toLowerCase().indexOf(in.charAt(i));
-            index = keyForMid.toLowerCase().indexOf(keyForOuter.toLowerCase().charAt(index));
-            encoded = encoded.replace(in.charAt(i), keyForOuter.toLowerCase().charAt(index));
-        }
+
         return encoded;
     }
     /*public String decode(String in, int wheel_O, int wheel_M, int wheel_I) {
