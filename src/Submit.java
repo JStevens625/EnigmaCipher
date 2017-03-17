6c1f63e79fd439204c446299a953e01ce2318f28
HaloMixter <mattsonlj@gmail.com>
2017-03-17 11:02:46 -0400

    Add files via upload

--- src/Submit.java
+++ src/Submit.java
@@ -24,7 +24,7 @@ import jdk.nashorn.internal.ir.BreakNode;
 public class Submit extends JPanel
 {
 
-    EncodeDecode ed = new EncodeDecode();
+    EncodeDecode ed = new EncodeDecode();
 
     private ActionListener submit = new ActionListener()
     {
@@ -36,11 +36,11 @@ public class Submit extends JPanel
                     JOptionPane.showMessageDialog(null, "Please Select encode or decode");
                 }
                 else {
-                    String nameit = JOptionPane.showInputDialog("What will the File name be? (Do not include file extensions)");
-                    File statText = new File("src/Coded/" + nameit + ".txt");
-                    FileOutputStream is = new FileOutputStream(statText);
-                    OutputStreamWriter osw = new OutputStreamWriter(is);
-                    Writer writing = new BufferedWriter(osw);
+                    String nameit = JOptionPane.showInputDialog("What will the File name be?");
+                    File statText = new File(nameit + ".txt");
+                    FileOutputStream is = new FileOutputStream(statText);
+                    OutputStreamWriter osw = new OutputStreamWriter(is);
+                    Writer writing = new BufferedWriter(osw);
                     if (ed.getSelect() == 1) {
                         writing.write("");
                     }
